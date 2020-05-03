f64640e3ebff98ba620ccddc889c9277d1d73df5
Nicholas Humfrey <njh@aelius.com>
2020-05-04 00:34:00 +0100

    Made 'publish' command work for both files and directories

---
+++ test/parse-filename.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# shellcheck source=delphyne
+source "${BASH_SOURCE%/*}/../delphyne"
+
+set +e
+set +u
+
+testParseZoneFilenameSimpleDotZone() {
+  assertEquals 'example.com' "$(parse_zone_filename 'example.com.zone')"
+}
+
+testParseZoneFilenameSimpleDbDot() {
+  assertEquals 'example.com' "$(parse_zone_filename 'db.example.com')"
+}
+
+testParseZoneFilenameTooShort() {
+  assertContains "$(parse_zone_filename 'example.zone' 2>&1)" 'Filename does not contain a valid zone name' 
+}
+
+testParseZoneFilenameInvalidCharacters() {
+  assertContains "$(parse_zone_filename 'example_.com.zone' 2>&1)" 'Filename does not contain a valid zone name' 
+}
+
+testParseZoneFilenameRelative() {
+  assertEquals 'example.com' "$(parse_zone_filename '../example.com.zone')"
+}
+
+testParseZoneFilenameFullPath() {
+  assertEquals 'example.com' "$(parse_zone_filename '/home/user/dns/example.com.zone')"
+}
+
+
+# shellcheck disable=SC1091
+source "$(command -v shunit2)"
